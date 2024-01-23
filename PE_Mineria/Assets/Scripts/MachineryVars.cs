eb3b7b14b1a0d67b13c714fbe3d64d7b55c7690f
Estilazo <estilazofreestyle@gmail.com>
2024-01-23 09:36:30 -0300

    pc change push

---
+++ PE_Mineria/Assets/Scripts/MachineryVars.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MachineryVars", menuName = "Game/Machinery Variables")]
+public class MachineryVars : ScriptableObject
+{
+
+}
